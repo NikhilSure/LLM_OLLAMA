d7641aa1d74b24a04362921e4b534cb0666985d0
Tejaswi-175 <tejaswi@vassarlabs.com>
2026-01-22 08:46:44 +0530

    Basic Spring Starter code

---
+++ src/main/java/com/nsure/LLM_OLLAMA/LlmOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.nsure.LLM_OLLAMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmOllamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsure/LLM_OLLAMA/LlmOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsure.LLM_OLLAMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
